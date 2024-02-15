507f17d32d3b566a91d178303889dacf33d71349
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-15 20:27:57 -0300

    cadastro de dados

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<?php
+    //Verifica se os dados foram submitados
+    if(isset($_POST['submit'])){
+        include_once("connection.php");
+
+        $nome = $_POST['nome'];
+        $cpf = $_POST['cpf'];
+        $email = $_POST['email'];
+        $telefone = $_POST['telefone'];
+        $senha = $_POST['senha'];
+        $dataNasc = $_POST['dataNasc'];
+
+        //Query de cadastro
+        $cadastro = mysqli_query($connection, "INSERT INTO usuarios(nome,cpf,email,telefone,senha,dataNasc) VALUES
+        ('$nome','$cpf','$email','$telefone','$senha','$dataNasc')");
+
+    }else{
+        
+    }
+
+?>

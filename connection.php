3d21e017a761dd0def1d5dfa064803d144578163
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-15 11:24:11 -0300

    atualização nome db

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $dbHost = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "projetoweb"; 
+$dbName = "projeto"; 
 
 $connection = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 

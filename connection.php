3a78456d128287190c2c0ceff0d918bfc7337865
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-15 19:55:39 -0300

    Conexão com banco de dados

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $dbHost = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "projeto"; 
+$dbName = "sistemacadastro"; 
 
 $connection = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 

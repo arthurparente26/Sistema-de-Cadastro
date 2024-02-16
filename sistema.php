ea206a18e0a74518e67e8dbfe148b2f1db9bcd43
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-16 18:40:10 -0300

    Direcionamento de paginas

--- sistema.php
+++ sistema.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/sistema.css">
     <title>Pagina Inicial</title>
 </head>
 <body>
     <h1>Bem Vindo ao Sistema !</h1>
     <h2>By Arthur Parente</h2>
     <div class = "box">
-        <a href="">Login</a>
-        <a href="">Cadastro</a>
+        <a href="login.html">Login</a>
+        <a href="cadastro.html">Cadastro</a>
     </div>
 </body>
 </html>

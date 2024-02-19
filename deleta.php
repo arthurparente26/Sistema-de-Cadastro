a5e054bc66047c6075838855244bc1a480a5daad
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-19 17:01:35 -0300

    Deleção de dados

--- deleta.php
+++ deleta.php
@@ -4,9 +4,11 @@
 
         $id = $_GET['cpf'];
 
+        //BUSCA CADASTROS COM OS DADOS INFORMADOS
         $sqlSelect = "SELECT * FROM usuarios where cpf=$id";
         $result = $connection->query($sqlSelect);
 
+        //CASO EXISTA ALGUM DADO EXCLUI
         if($result->num_rows > 0){
             $sqlDelete = "DELETE FROM usuarios WHERE cpf =$id";
             $resultDelete = $connection->query($sqlDelete);

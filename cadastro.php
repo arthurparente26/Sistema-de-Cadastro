4656bd87a184930151f6da30805f32f883c0aee8
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-19 17:01:18 -0300

    Cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,9 @@
 <?php
+    //VERIFICA SE OS DADOS FORAM ENVIADOS PELO BOTÃO
     if(isset($_POST['submit'])){
         include_once('connection.php');
 
+        //GUARDA VALORES DIGITADOS EM VARIAVES
         $nome = $_POST['nome'];
         $cpf = $_POST['cpf'];
         $email = $_POST['email'];
@@ -9,6 +11,7 @@
         $senha = $_POST['senha'];
         $dataNasc = $_POST['dataNasc'];
 
+        //QUERY PARA CADASTRO
         $cadastroQuery = mysqli_query($connection, "INSERT INTO usuarios(nome,cpf,email,telefone,senha,dataNasc) 
         VALUES ('$nome','$cpf','$email','$telefone','$senha','$dataNasc')");
     }

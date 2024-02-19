289dd331eaf0eda1f51e8fd4cbef41fb2848218c
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-19 17:02:16 -0300

    Adicionado pesquisa de dados

--- sistema.php
+++ sistema.php
@@ -9,9 +9,17 @@
         header("Location: login.php");
     }
 
+    //GUARDA EMAIL EM VARIAVEL PARA MENSAGEM DE BOAS VINDAS
     $logado = $_SESSION['email'];
-
-    $sql = "SELECT * FROM usuarios ORDER BY nome DESC";
+    
+    //VERIFICA SE HÁ PESQUISA DE DADOS CASO NÃO EXISTA APRESENTA TODOS OS CADASTROS
+    if (!empty($_GET['search'])) {
+        $data = $_GET['search'];
+        $sql = "SELECT * FROM usuarios WHERE cpf LIKE '%$data%' or nome LIKE '%$data%' or 
+        email LIKE '%$data%' ORDER BY cpf DESC";
+    }else{
+        $sql = "SELECT * FROM usuarios ORDER BY nome DESC";
+    }
 
     $result = $connection->query($sql);
 
@@ -42,6 +50,17 @@
     <?php
         echo "<h1 style='color:white;text-align: center;'>Bem Vindo <u>$logado</u></h1>";
     ?>
+
+    <div class="box-search" style= "display: flex;
+    justify-content: center;
+    gap: .1%;">
+        <input type="search" class="form-control w-25" placeholder="Pesquisar" id = "pesquisar">
+        <button onclick = "searchData()" class = "btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+        </svg>
+        </button>
+    </div>
 
     <div class="m-5">
         <table class="table text-white table-bg">
@@ -84,4 +103,8 @@
             </tbody>
         </table>
     </div>
+
+    <!-- ARQUIVO JS PARA FAZER PESQUISA -->
+    <script src="js/script.js"></script>
+
 </body>
